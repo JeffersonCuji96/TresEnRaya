77c43b5a7fbb3777c7b07482263d70e1faa257bb
JeffersonCuji96 <renemarin96jc@gmail.com>
2022-01-26 13:02:24 -0500

    Se agrega métodos en el hub para manejar el inicio y finalización del juego y los turnos

--- TresEnRaya/Hubs/GameHub.cs
+++ TresEnRaya/Hubs/GameHub.cs
@@ -3,11 +3,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TresEnRaya.Data;
+using TresEnRaya.Extensions;
 
 namespace TresEnRaya.Hubs
 {
     public class GameHub:Hub
     {
+        readonly GameService _service = new GameService();
+        const string X = "X";
+        const string O = "O";
 
+        public async Task NuevoJuego(Game game)
+        {
+            game = await _service.EstablecerTableroJuego();
+            await Clients.All.SendAsync(HubMessages.NuevoJuego, game);
+        }
+        public async Task EnvioMensaje(Game game)
+        {
+            await Clients.All.SendAsync(HubMessages.RecibeMensaje,game);
+        }
+
+        public async Task GestionarJuegoFinalizado(Game game)
+        {
+            if (game.Juego.GanadorX())
+            {
+                game.XGanador = true;
+                game.JuegoFinalizado = true;
+            }else if (game.Juego.GanadorO())
+            {
+                game.OGanador = true;
+                game.JuegoFinalizado = true;
+            }else if (game.Juego.Empatado())
+            {
+                game.EmpateJuego = true;
+                game.JuegoFinalizado = true;
+            }
+            if (game.JuegoFinalizado)
+            {
+                game.Jugador1Turno = false;
+                game.Jugador2Turno = false;
+                await Clients.All.SendAsync(HubMessages.JuegoFinalizado, game);
+            }
+        }
+
+        public async Task FinalizarTurnoJugador(Game game)
+        {
+            await GestionarJuegoFinalizado(game);
+            if (!game.JuegoFinalizado)
+            {
+                game.TurnoJugador++;
+                game.Jugador1Turno = !game.Jugador1Turno;
+                game.Jugador2Turno = !game.Jugador2Turno;
+                await Clients.All.SendAsync(HubMessages.TurnoFinalizado, game);
+            }
+        }
+        public async Task TomarTurnoJugador(int i, Game game)
+        {
+            if (game.Jugador1Turno)
+            {
+                game.Juego[i] = X;
+            }
+            else if(game.Jugador2Turno)
+            {
+                game.Juego[i] = O;
+            }
+            await FinalizarTurnoJugador(game);
+        }
     }
 }
